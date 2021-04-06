f3d3f17a804f6041a5ec8c8dfd78f120ad4cb439
jaffarabbas <gamaportal8@gmail.com>
2021-04-06 22:37:16 +0500

    6-april-2021    C# initial practice projects

---
+++ Exploring/2dMatrixs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _2dMatrixs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3,3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    Console.WriteLine("Please enter number i : "+i+" x "+x);
+                    arr[x, i] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    Console.Write(arr[x, i] + " ");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
