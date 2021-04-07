2c9e1fe48281e853615bda8b1b9a246d25ddd19b
jaffarabbas <gamaportal8@gmail.com>
2021-04-07 16:44:43 +0500

    4-7-2921

--- Practise/pr1/Program.cs
+++ Practise/pr1/Program.cs
@@ -2,11 +2,21 @@
 
 namespace pr1
 {
+    class code{
+        //printing hello word 100 times without using loop
+        public static void print100(int num){
+            if(num<=100){
+                Console.WriteLine("Helo Jaffar");
+                num++;
+                print100(num);
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            code.print100(1);
         }
     }
 }
