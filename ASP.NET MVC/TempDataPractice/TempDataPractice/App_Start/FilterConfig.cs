e03d2c391e9c0535e9393f1e3c6cd90775bad527
jaffarabbas <gamaportal8@gmail.com>
2021-04-13 20:14:26 +0500

    4-13-2021   TempData Practice

---
+++ ASP.NET MVC/TempDataPractice/TempDataPractice/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TempDataPractice
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ASP.NET MVC/TempDataPractice/TempDataPractice/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TempDataPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            TempData["Data"] = "New Data here ";
+            int[] arr = { 1, 3, 4, 5, 6, 7 };
+            TempData["arry"] = arr;
+
+            TempData.Keep();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            if (TempData["Data"] != null)
+            {
+                TempData["Data"].ToString();
+                TempData.Keep();
+            }
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            if (TempData["Data"] != null)
+            {
+                TempData["Data"].ToString();
+                TempData.Keep();
+            }
+            return View();
+        }
+    }
+}
