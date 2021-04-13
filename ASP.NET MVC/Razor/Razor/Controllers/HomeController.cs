6fb19b0c288ae1ade2a5bc965e4b1f672493c892
jaffarabbas <gamaportal8@gmail.com>
2021-04-13 18:42:09 +0500

    4-13-2021  ViewData ViewBag Practice

--- ASP.NET MVC/Razor/Razor/Controllers/HomeController.cs
+++ ASP.NET MVC/Razor/Razor/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace Razor.Controllers
 
             ViewData["Comit"] = "New COmmit";
 
+            int[] arr = { 1, 3, 4, 5, 6 };
+
+            ViewBag.Arr = arr;
+            ViewData["Arr"] = arr;
+
             return View();
         }
 
