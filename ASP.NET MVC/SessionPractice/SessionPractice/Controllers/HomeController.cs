ebee49258996b80c1b698bc0ed4a1abe83fd1ffd
jaffarabbas <gamaportal8@gmail.com>
2021-04-13 20:37:56 +0500

    4-13-2021   SessionData Practice

---
+++ ASP.NET MVC/SessionPractice/SessionPractice/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SessionPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        string[] arr = { "sdfsdf", "dsfsdf", "sdgdfgdf" };
+        public ActionResult Index()
+        {
+            Session["first"] = "Hello Jaffar";
+            Session["arr"] = arr;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            if (Session["first"] != null || Session["arr"] !=null) 
+            {
+                Session["first"].ToString();
+                Session["arr"] = arr;
+            }
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            if (Session["first"] != null || Session["arr"] != null)
+            {
+                Session["first"].ToString();
+                Session["arr"] = arr;
+            }
+            return View();
+        }
+    }
+}
