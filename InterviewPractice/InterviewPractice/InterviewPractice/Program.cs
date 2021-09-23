b13de5fc778cac01ebc98823b9364b8af85682d0
jaffarabbas <fa19bsse0008@maju.edu.pk>
2021-09-23 19:23:05 +0500

    23/09/2021 19:23:01

---
+++ InterviewPractice/InterviewPractice/InterviewPractice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InterviewPractice
+{
+    class check
+    {
+        private int a = 4;
+
+        public int A { get => a; set => a = value; }
+    }
+    class check1 : check
+    {
+        public void prints()
+        {
+            A = 3;
+            Console.WriteLine(A);
+        }
+    }
+
+
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            check1 obj = new check1();
+            obj.prints();
+        }
+    }
+}
