5c6af06f429576962dc930e75f8a7ac7f25b652a
jaffarabbas <gamaportal8@gmail.com>
2021-10-24 00:13:35 +0500

    24/10/2021 00:13:32

---
+++ Console-apps/rider_test_1/rider_test_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rider_test_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("dsfsdf");
+        }
+    }
+}
