848f08e3226571202f164232094f93d4a7d52d3e
jaffarabbas <gamaportal8@gmail.com>
2021-10-26 21:03:54 +0500

    26/10/2021 21:03:41

---
+++ Console-apps/rider_test_web_1/rider_test_web_1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rider_test_web_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            TempData["check"] = "hel";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
