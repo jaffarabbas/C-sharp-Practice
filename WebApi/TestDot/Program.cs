cd520f1dfbebe16c3005221f3dde2db05c3537bd
jaffarabbas <gamaportal8@gmail.com>
2021-11-02 19:49:03 +0500

    02/11/2021 19:48:51

---
+++ WebApi/TestDot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestDot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
