3f9133a7bb6a0d6a0a0b5c9bd54c6f4c38dd74e8
jaffarabbas <gamaportal8@gmail.com>
2021-11-02 19:48:51 +0500

    02/11/2021 19:48:51

---
+++ Practise/TestDot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestDot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] arr = {1,2,3};
+           foreach (int i in arr){
+               Console.WriteLine(i);
+           }
+        }
+    }
+}
