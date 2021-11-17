13ffed70b2bc8944b7a2c9fd98722991b271d46a
jaffarabbas <gamaportal8@gmail.com>
2021-11-17 18:19:39 +0500

    17/11/2021 18:19:21

---
+++ Console-apps/testEntity/testEntity/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using testEntity.Models;
+
+namespace testEntity.Controllers
+{
+    public class HomeController : Controller
+    {
+        Data obj = new Data();
+        public ActionResult Index()
+        {
+            obj.NAME = "jaffar";
+            obj.age = 4;
+            List<Data> lst = new List<Data>();
+            lst.Add(obj);
+            return View(lst);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
