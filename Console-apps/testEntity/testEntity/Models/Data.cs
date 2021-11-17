4e84470fa3a4808da4c5d9d46c820fa487da2f65
jaffarabbas <gamaportal8@gmail.com>
2021-11-17 18:19:54 +0500

    17/11/2021 18:19:21

--- Console-apps/testEntity/testEntity/Models/Data.cs
+++ Console-apps/testEntity/testEntity/Models/Data.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
 namespace testEntity.Models
 {
     public class Data
     {
-        
+        public string NAME { get; set; }
+        public int age  { get; set; }
     }
 }
