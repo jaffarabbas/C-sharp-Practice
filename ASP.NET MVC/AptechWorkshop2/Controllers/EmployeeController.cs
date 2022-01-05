0e4fe1ccbff4f69b55ea7665149e80c44c3dbe5a
Jaffar Abbas <gamaportal8@gmail.com>
2022-01-05 21:46:57 +0500

    05/01/2022 21:46:50

---
+++ ASP.NET MVC/AptechWorkshop2/Controllers/EmployeeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AptechWorkshop2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public readonly Models.Database.EmployeeContext _context;
+        public EmployeeController(Models.Database.EmployeeContext context) {
+            _context = context;
+        }
+        [HttpGet("get")]
+        public IActionResult Get() { return Ok("OK"); }
+    }
+}
