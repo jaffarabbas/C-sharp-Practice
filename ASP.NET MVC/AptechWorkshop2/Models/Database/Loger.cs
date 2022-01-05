64ab7f8712a042b0abcaa0e9b451ba1a603fd915
Jaffar Abbas <gamaportal8@gmail.com>
2022-01-05 21:47:00 +0500

    05/01/2022 21:46:50

---
+++ ASP.NET MVC/AptechWorkshop2/Models/Database/Loger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace AptechWorkshop2.Models.Database
+{
+    [Keyless]
+    [Table("loger")]
+    public partial class Loger
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Unicode(false)]
+        public string? Loggerfile { get; set; }
+    }
+}
