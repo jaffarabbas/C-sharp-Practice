91cb7d568fe91c03a1be1006a3590244f40b074e
Jaffar Abbas <gamaportal8@gmail.com>
2022-01-05 21:46:58 +0500

    05/01/2022 21:46:50

---
+++ ASP.NET MVC/AptechWorkshop2/Models/Database/Address.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace AptechWorkshop2.Models.Database
+{
+    [Table("ADDRESS")]
+    public partial class Address
+    {
+        public Address()
+        {
+            Employees = new HashSet<Employee>();
+        }
+
+        [Key]
+        [Column("AD_ID")]
+        public int AdId { get; set; }
+        [Column("ADD_DETIALS")]
+        [StringLength(50)]
+        [Unicode(false)]
+        public string? AddDetials { get; set; }
+        [Column("ADD_STATE")]
+        [StringLength(40)]
+        [Unicode(false)]
+        public string? AddState { get; set; }
+        [Column("ADD_COUNTRY")]
+        [StringLength(40)]
+        [Unicode(false)]
+        public string? AddCountry { get; set; }
+
+        [InverseProperty(nameof(Employee.EmpAddress))]
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
