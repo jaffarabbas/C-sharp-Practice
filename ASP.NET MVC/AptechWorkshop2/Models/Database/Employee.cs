d70b5f3cb934732c9276d2100681f7b0d0b23dab
Jaffar Abbas <gamaportal8@gmail.com>
2022-01-05 21:46:59 +0500

    05/01/2022 21:46:50

---
+++ ASP.NET MVC/AptechWorkshop2/Models/Database/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace AptechWorkshop2.Models.Database
+{
+    [Table("EMPLOYEE")]
+    [Index(nameof(EmpEmail), Name = "UQ__EMPLOYEE__19388119B1EC796D", IsUnique = true)]
+    public partial class Employee
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Column("EMP_NAME")]
+        [StringLength(20)]
+        [Unicode(false)]
+        public string EmpName { get; set; } = null!;
+        [Column("EMP_EMAIL")]
+        [StringLength(40)]
+        [Unicode(false)]
+        public string EmpEmail { get; set; } = null!;
+        [Column("EMP_ADDRESS_ID")]
+        public int EmpAddressId { get; set; }
+        [Column("EMP_CODE")]
+        [StringLength(50)]
+        [Unicode(false)]
+        public string EmpCode { get; set; } = null!;
+
+        [ForeignKey(nameof(EmpAddressId))]
+        [InverseProperty(nameof(Address.Employees))]
+        public virtual Address EmpAddress { get; set; } = null!;
+    }
+}
