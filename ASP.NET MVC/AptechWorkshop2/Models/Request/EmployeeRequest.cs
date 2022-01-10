ccd9b162b35a882a856fb17644cd2e2a7e55a42e
Jaffar Abbas <gamaportal8@gmail.com>
2022-01-10 21:29:57 +0500

    10/01/2022 21:29:55

---
+++ ASP.NET MVC/AptechWorkshop2/Models/Request/EmployeeRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace AptechWorkshop2.Models.Request
+{
+    public class EmployeeRequest
+    {
+        public string empname { get; set; }
+
+        public string empemail { get; set; }
+
+        public int addressid { get; set; }
+
+        public string empcode { get; set; }
+
+        public string empdetails { get; set; }
+
+        public string empstate { get; set; }
+
+        public string empcountry { get; set; }
+    }
+}
