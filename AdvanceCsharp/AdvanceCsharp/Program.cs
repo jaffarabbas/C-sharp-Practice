494eebd5a1b37468abe06a9aeb2c1112a9f1ddc5
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-05 19:14:04 +0500

    05/04/2022 19:13:55

---
+++ AdvanceCsharp/AdvanceCsharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdvanceCsharp.Classes;
+namespace AdvanceCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //we cannot make obj of a abstract class
+            //AbstractClasses obj = new AbstractClasses();
+            Child obj = new Child();
+            obj.print();
+            obj.AbMethod();
+        }
+    }
+}
