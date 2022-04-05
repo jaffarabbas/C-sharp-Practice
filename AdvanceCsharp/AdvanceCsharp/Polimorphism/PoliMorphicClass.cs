90ac4ccc66912f74ded79a8945ce0797144df118
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-05 20:10:44 +0500

    05/04/2022 20:10:42

---
+++ AdvanceCsharp/AdvanceCsharp/Polimorphism/PoliMorphicClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceCsharp.Polimorphism
+{
+    public class PoliMorphicClass
+    {
+        public virtual void tester()
+        {
+            Console.WriteLine("Test");
+        }
+        public void overLoad()
+        {
+            Console.WriteLine("no over");
+        }
+    }
+    public class ChildClass : PoliMorphicClass
+    {
+        public override void tester()
+        {
+            Console.WriteLine("Child test");
+        }
+
+        public new void overLoad()
+        {
+            Console.WriteLine("yes over");
+        }
+
+        public void overLoad(int a)
+        {
+            Console.WriteLine("yes over {0}",a);
+        }
+    }
+}
