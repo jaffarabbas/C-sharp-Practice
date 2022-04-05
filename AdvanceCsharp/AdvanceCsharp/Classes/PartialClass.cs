1b9aeb1387b83b8a569045861cac12dc7c2f53aa
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-05 20:10:43 +0500

    05/04/2022 20:10:42

---
+++ AdvanceCsharp/AdvanceCsharp/Classes/PartialClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceCsharp.Classes
+{
+    partial class PartialClass
+    {
+        private int id { get; set; }
+        private string name { get; set; }
+    }
+
+    partial class PartialClass
+    {
+        List<PartialClass> partialClasses = new List<PartialClass>();
+        public void add(int id ,string name)
+        {
+            partialClasses.Add(new PartialClass() { id = id , name = name });
+        }
+        public void view()
+        {
+            foreach (PartialClass partialClass in partialClasses)
+            {
+                Console.WriteLine("{0} {1}",partialClass.id,partialClass.name);
+            }
+        }
+    }
+}
