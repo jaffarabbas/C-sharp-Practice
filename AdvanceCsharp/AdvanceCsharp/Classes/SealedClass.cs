8dacad6d46c9a78cc532700d377d27fea1178992
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-05 20:10:44 +0500

    05/04/2022 20:10:42

---
+++ AdvanceCsharp/AdvanceCsharp/Classes/SealedClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceCsharp.Classes
+{
+    sealed class SealedClass
+    {
+        private int fname;
+        public int Fname
+        {
+            get { return fname; }
+            set { fname = value; }
+        }
+    }
+
+    //class test : SealedClass
+    //{
+
+    //}
+}
