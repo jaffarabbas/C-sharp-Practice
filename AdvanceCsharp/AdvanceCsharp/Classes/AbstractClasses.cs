3f704cd55a72ea3ebd60a0ef52fd01ad1033d106
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-05 19:14:03 +0500

    05/04/2022 19:13:55

---
+++ AdvanceCsharp/AdvanceCsharp/Classes/AbstractClasses.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceCsharp.Classes
+{
+    abstract class AbstractClasses : InterfaceClass
+    {
+        public int value = 3;
+        public string name { get; set; } = String.Empty;
+        public abstract int AbMethod();
+
+        public void getname()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setname()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Child : AbstractClasses
+    {
+        public void print()
+        {
+            Console.WriteLine(value);
+        }
+
+        public override int AbMethod()
+        {
+            return value;
+        }
+    }
+}
