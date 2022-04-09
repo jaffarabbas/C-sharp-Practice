d9c12f9f527e34a844e13439006dbcf480ce8f56
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-09 16:39:05 +0500

    09/04/2022 16:39:03

---
+++ AdvanceCsharp/AdvanceCsharp/Delegates/DelegatesClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceCsharp.Delegates
+{
+    public delegate void MyDelegate(string msg);
+    public class DelegatesClass
+    {
+        public static void TestDelegates()
+        {
+            MyDelegate myDelegate = MethodA;
+            InvokeDelegate(myDelegate);
+        }
+
+        public static void InvokeDelegate(MyDelegate del)
+        {
+            del("Hello World");
+        }
+
+        public static void MethodA(string message)
+        {
+            Console.WriteLine("Called ClassA.MethodA() with parameter: " + message);
+        }
+    }
+}
