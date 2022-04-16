8eb9c136ffd5e73f5665d6f4c015bd30f39ccbd4
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-16 21:53:08 +0500

    Inserted CsharpLeetCode/CsharpLeetCode/Palindrome Number.cs at 16/04/2022 21:53:07

---
+++ Leetcode/CsharpLeetCode/CsharpLeetCode/Palindrome Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLeetCode
+{
+    public class Palindrome_Number
+    {
+        public bool IsPalindrome(int x)
+        {
+            return x.ToString().SequenceEqual(x.ToString().Reverse());
+        }
+
+        static void Main(string[] args)
+        {
+            Palindrome_Number p = new Palindrome_Number();
+            bool a  = p.IsPalindrome(121);
+            Console.WriteLine(a);
+            Console.Read();
+        }
+    }
+}
