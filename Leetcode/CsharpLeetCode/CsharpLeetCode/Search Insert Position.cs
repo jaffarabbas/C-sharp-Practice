1116562591c300210548a414545684bae19f354c
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-17 19:33:42 +0500

    Inserted CsharpLeetCode/CsharpLeetCode/Search Insert Position.cs at 17/04/2022 19:33:41

---
+++ Leetcode/CsharpLeetCode/CsharpLeetCode/Search Insert Position.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLeetCode
+{
+    public class Search_Insert_Position
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                if (nums[i] == target) return i;
+            for (int i = 0; i < nums.Length - 1; i++)
+                if (nums[i] < target && nums[i + 1] > target) return i + 1;
+            if (target < nums[0]) return 0;
+            else return nums.Length;
+        }
+        static void Main(string[] args)
+        {
+            Roman_to_Integer p = new Roman_to_Integer();
+            int a = p.RomanToInt("IV");
+            Console.WriteLine(a);
+            Console.Read();
+        }
+
+    }
+}
