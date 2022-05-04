582dae3169302dacee758c5f923bea22e4497628
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-04 23:46:16 +0500

    Updated AdvanceCsharp/Classes/StaticClass.cs at 04/05/2022 23:46:16

--- AdvanceCsharp/AdvanceCsharp/Classes/StaticClass.cs
+++ AdvanceCsharp/AdvanceCsharp/Classes/StaticClass.cs
@@ -8,11 +8,15 @@ namespace AdvanceCsharp.Classes
 {
     public class StaticClass
     {
-        public static Dictionary<string, string> c(string a,string b)
+        public static Dictionary<string, string> dic = new Dictionary<string, string>();
+        public static void c(string a,string b)
         {
-            Dictionary<string, string> ab = new Dictionary<string, string>();
-            ab.Add(a, b);
-            return ab;
+            dic.Add(a, b);
+        }
+
+        public static Dictionary<string,string> getDictionary()
+        {
+            return dic;
         }
     }
 }
