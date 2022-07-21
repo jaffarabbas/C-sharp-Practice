732a1026b74a9ed0b105cc50619b31effbbba12a
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-21 23:59:49 +0500

    Inserted GitCommiter/Form1.cs at 21/07/2022 23:59:49

---
+++ GitCommiter/GitCommiter/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GitCommiterMethodTester;
+
+namespace GitCommiter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillList();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            SelectRepo(e);
+            PopulateListBoxWithStageChanges();
+        }
+
+        private void btnCommit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Methods
+
+        private void FillList()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("ID", typeof(int));
+            table.Columns.Add("Repo");
+            foreach (var data in Commiter.GitPathList())
+            {
+                table.Rows.Add(new object[] { data.Key, data.Value });
+            }
+            gvDataView.DataSource = table;
+            gvDataView.Columns[0].Width = 20;
+        }
+
+        private void SelectRepo(DataGridViewCellEventArgs e)
+        {
+            repoPathLabel.Text = gvDataView.Rows[e.RowIndex].Cells[1].Value.ToString();
+        }
+
+        private void PopulateListBoxWithStageChanges()
+        {
+            lbCommitDetails.Items.Clear();
+            List<string> dataList = Commiter.StageChanges(repoPathLabel.Text);
+            BindingSource bindingSource = new BindingSource();
+            if (dataList.Count != 0)
+            {
+                foreach (var item in dataList)
+                {
+                    lbCommitDetails.Items.Add(item.ToString());
+                }
+            }
+            else
+            {
+                dataList.Add("No Changes In Repository");
+                lbCommitDetails.Items.Add(dataList.FirstOrDefault().ToString());
+            }
+            bindingSource.DataSource = dataList;
+        }
+
+        #endregion
+
+    }
+}
