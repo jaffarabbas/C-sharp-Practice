c759fde8bcd9d1e7272621d4ab8f057d67d47969
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-23 14:38:02 +0500

    Commited File : Form1.cs at 7/23/2022 2:38:02 PM

--- GitCommiter/GitCommiter/Form1.cs
+++ GitCommiter/GitCommiter/Form1.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using GitCommiterMethodTester;
+using GitCommiterMethods;
 
 namespace GitCommiter
 {
