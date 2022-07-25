d5411ef450d3b785fe3050120e660921a355c139
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-26 00:21:28 +0500

    Commited File : EmployeeDataController.cs at 7/26/2022 12:21:27 AM

---
+++ WebAPITest/WebAPITest/Controllers/EmployeeDataController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPITest.Controllers
+{
+    public class EmployeeDataController : ApiController
+    {
+        Random random = new Random();
+        public List<string> employee = new List<string>();
+        public EmployeeDataController()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                employee.Add(random.Next(1000).ToString());
+            }
+        }
+
+        [HttpGet]
+        public List<string> GetEmployee()
+        {
+            return employee;
+        }
+
+        [HttpGet]
+        public string GetEmployee(int id)
+        {
+            return employee.ElementAt(id);
+        }
+    }
+}
