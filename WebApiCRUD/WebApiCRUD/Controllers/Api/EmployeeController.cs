9810aa6db63c419425ee33cb31b03f8f3ee2393b
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-27 14:29:57 +0500

    Commited File : EmployeeController.cs at 7/27/2022 2:29:57 PM

---
+++ WebApiCRUD/WebApiCRUD/Controllers/Api/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiCRUD.Models;
+
+namespace WebApiCRUD.Controllers.Api
+{
+    public class EmployeeController : ApiController
+    {
+        DB_WEB_API_CRUDEntities modelConnection = new DB_WEB_API_CRUDEntities();
+
+        [HttpGet]
+        public IHttpActionResult GetEmployee()
+        {
+            List<Employee> employees = modelConnection.Employees.ToList();
+            return Ok(employees);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetEmployee(int id)
+        {
+            var employees = modelConnection.Employees.Where(model => model.id == id).FirstOrDefault();
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public IHttpActionResult PostEmployee(Employee employee)
+        {
+            modelConnection.Employees.Add(employee);
+            modelConnection.SaveChanges();
+            return Ok();
+        }
+    }
+}
