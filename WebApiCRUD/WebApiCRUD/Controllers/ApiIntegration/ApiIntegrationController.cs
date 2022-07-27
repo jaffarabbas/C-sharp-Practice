6bb6fd17540ae9af6e54bd11b41df93b62a05057
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-27 14:29:59 +0500

    Commited File : ApiIntegrationController.cs at 7/27/2022 2:29:58 PM

---
+++ WebApiCRUD/WebApiCRUD/Controllers/ApiIntegration/ApiIntegrationController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows.Forms;
+using APIHandler;
+using WebApiCRUD.Models;
+
+namespace WebApiCRUD.Controllers.ApiIntegration
+{
+    public class ApiIntegrationController : Controller
+    {
+        ApiHandler apiHandler;
+        private string apiPath = "https://localhost:44316/api/Employee";
+        private string apiController = "Employee";
+        // GET: ApiIntegration
+        public ActionResult Index()
+        {
+            apiHandler = new ApiHandler();
+            var empData = apiHandler.GetData(apiPath, apiController).Select(model => new Employee { 
+                id = model.id,
+                name = model.name,
+                age = model.age,
+                designation = model.designation,
+                gender = model.gender,
+                salary = model.salary 
+            });
+            return View(empData);
+        }
+
+        public ActionResult Create(Employee employee)
+        {
+            apiHandler = new ApiHandler();
+            if (apiHandler.PostData(apiPath, employee, apiController))
+            {
+                return RedirectToAction("Index");
+            }
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(apiPath);
+            var response = httpClient.PostAsJsonAsync<Employee>(apiController, employee);
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
