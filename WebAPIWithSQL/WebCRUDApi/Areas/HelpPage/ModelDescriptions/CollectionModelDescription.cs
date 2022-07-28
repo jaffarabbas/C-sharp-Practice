76fa334508f5291f2c143c50ab00db9778e4ac7e
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-28 13:36:58 +0500

    Inserted WebCRUDApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs at 28/07/2022 13:36:57

---
+++ WebAPIWithSQL/WebCRUDApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebCRUDApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
