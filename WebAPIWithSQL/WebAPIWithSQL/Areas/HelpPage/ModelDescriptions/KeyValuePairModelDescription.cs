be36af2f53e02d5f08a5e20b7fb007ec896003bc
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-28 13:35:40 +0500

    Inserted WebAPIWithSQL/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs at 28/07/2022 13:35:39

---
+++ WebAPIWithSQL/WebAPIWithSQL/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace WebAPIWithSQL.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
