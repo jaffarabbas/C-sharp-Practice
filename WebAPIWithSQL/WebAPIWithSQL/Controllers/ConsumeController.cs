d422b7e45d154732f5c7601032c5663a698f8165
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-28 13:36:09 +0500

    Inserted WebAPIWithSQL/Controllers/ConsumeController.cs at 28/07/2022 13:36:08

---
+++ WebAPIWithSQL/WebAPIWithSQL/Controllers/ConsumeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebAPIWithSQL.Models;
+
+namespace WebAPIWithSQL.Controllers
+{
+    public class ConsumeController : Controller
+    {
+        HttpClient httpClient = new HttpClient();
+
+        public List<STUDENT> GetData()
+        {
+            httpClient.BaseAddress = new Uri("https://localhost:44309/api/StudentApi");
+            var response = httpClient.GetAsync("StudentApi");
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<STUDENT>>();
+                display.Wait();
+                return display.Result;
+            }
+            return null;
+        }
+        // GET: Consume
+        public ActionResult Index()
+        {
+            return View(GetData());
+        }
+    }
+}
