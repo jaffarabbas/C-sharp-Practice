6a985665f261bea4ae5dd9af98b8c1e110a90a40
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-28 13:36:08 +0500

    Inserted WebAPIWithSQL/Controllers/Api/StudentApiController.cs at 28/07/2022 13:36:08

---
+++ WebAPIWithSQL/WebAPIWithSQL/Controllers/Api/StudentApiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIWithSQL.Models;
+
+namespace WebAPIWithSQL.Controllers.Api
+{
+    public class StudentApiController : ApiController
+    {
+        DB_TestEntities dB_Test = new DB_TestEntities();
+
+        [HttpGet]
+        public IHttpActionResult Index()
+        {
+            List<STUDENT> data = dB_Test.STUDENTS.ToList(); 
+            return Ok(data);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Index(int id)
+        {
+            var data = dB_Test.STUDENTS.Where(d => d.ID == id).FirstOrDefault();
+            return Ok(data);
+        }
+    }
+}
