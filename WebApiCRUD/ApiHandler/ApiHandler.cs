d70e2fa29ce670b07a8fb6fdba9bc13d7537be75
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-30 18:29:36 +0500

    Commited File : ApiHandler.cs at 7/30/2022 6:29:36 PM

--- WebApiCRUD/ApiHandler/ApiHandler.cs
+++ WebApiCRUD/ApiHandler/ApiHandler.cs
@@ -10,8 +10,18 @@ namespace APIHandler
 {
     public class ApiHandler
     {
+        private string apiUrl;
+        private string controller;
+
+        public ApiHandler(string apiUrl, string controller)
+        {
+            this.apiUrl = apiUrl ?? throw new ArgumentNullException(nameof(apiUrl));
+            this.controller = controller ?? throw new ArgumentNullException(nameof(controller));
+        }
+
         private HttpClient httpClient;
-        public List<dynamic> GetData(string apiUrl,string controller)
+        
+        public IEnumerable<T> GetData<T>()
         {
             try
             {
@@ -20,13 +30,10 @@ namespace APIHandler
                 var response = httpClient.GetAsync(controller);
                 response.Wait();
                 var test = response.Result;
-                if (test.IsSuccessStatusCode)
-                {
-                    var display = test.Content.ReadAsAsync<List<dynamic>>();
-                    display.Wait();
-                    return display.Result;
-                }
-                return null;
+                if (!test.IsSuccessStatusCode) return null;
+                var display = test.Content.ReadAsAsync<List<T>>();
+                display.Wait();
+                return display.Result;
             }
             catch(HttpRequestException error)
             {
@@ -34,7 +41,39 @@ namespace APIHandler
             }
         }
         
-        public bool PostData<T>(string apiUrl, T obj,string controller)
+        /// <summary>
+        /// Get spesific data from database api with id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T GetData<T>(int id)
+        {
+            try
+            {
+                httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri(apiUrl);
+                var response = httpClient.GetAsync(controller+"?id="+id.ToString());
+                response.Wait();
+                var test = response.Result;
+                if (!test.IsSuccessStatusCode) return (dynamic)null;
+                var display = test.Content.ReadAsAsync<T>();
+                display.Wait();
+                return display.Result;
+            }
+            catch(HttpRequestException error)
+            {
+                return (dynamic)error.Message.ToList();
+            }
+        }
+        
+        /// <summary>
+        /// Insert Data to database api
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public bool PostData<T>(T obj)
         {
             try
             {
@@ -43,11 +82,30 @@ namespace APIHandler
                 var response = httpClient.PostAsJsonAsync<T>(controller,obj);
                 response.Wait();
                 var test = response.Result;
-                if (test.IsSuccessStatusCode)
-                {
-                    return true;
-                }
-                return false;
+                return test.IsSuccessStatusCode;
+            }
+            catch (HttpRequestException error)
+            {
+                return (dynamic)error.Message;
+            }
+        }
+
+        /// <summary>
+        /// Put data (update) data to database api
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public bool PutData<T>(T obj)
+        {
+            try
+            {
+                httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri(apiUrl);
+                var response = httpClient.PutAsJsonAsync<T>(controller, obj);
+                response.Wait();
+                var test = response.Result;
+                return test.IsSuccessStatusCode;
             }
             catch (HttpRequestException error)
             {
