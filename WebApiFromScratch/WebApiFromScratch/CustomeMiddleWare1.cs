1d66c99152783881beeaebf0b709c0b7585608ed
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-31 15:00:28 +0500

    Commited File : CustomeMiddleWare1.cs at 7/31/2022 3:00:27 PM

---
+++ WebApiFromScratch/WebApiFromScratch/CustomeMiddleWare1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace WebApiFromScratch
+{
+    public class CustomeMiddleWare1 : IMiddleware
+    {
+        public Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
