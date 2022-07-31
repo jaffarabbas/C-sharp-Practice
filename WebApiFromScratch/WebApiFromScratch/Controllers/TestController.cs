401a0b307d6a67a5a71aaea942607d799cba0345
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-31 13:04:08 +0500

    Commited File : TestController.cs at 7/31/2022 1:04:06 PM

---
+++ WebApiFromScratch/WebApiFromScratch/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiFromScratch.Controllers
+{
+    [ApiController]
+    [Route("test/[action]")]
+    public class TestController : ControllerBase
+    {
+        public string Get()
+        {
+            return "sadas";
+        }
+    }
+}
