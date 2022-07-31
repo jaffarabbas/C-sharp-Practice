405b8fcf6bca394d21b76a9fcd6664dde4942e13
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-31 13:04:09 +0500

    Commited File : Program.cs at 7/31/2022 1:04:08 PM

---
+++ WebApiFromScratch/WebApiFromScratch/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace WebApiFromScratch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateDefaultBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateDefaultBuilder(string[] args) =>
+        
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
