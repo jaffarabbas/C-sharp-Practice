100bfc0ffa14a4a623eacada389717c9e46719bd
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-31 13:04:11 +0500

    Commited File : Startup.cs at 7/31/2022 1:04:10 PM

---
+++ WebApiFromScratch/WebApiFromScratch/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebApiFromScratch
+{
+    public class Startup
+    {
+        public void ConfigureService(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("asdsad");
+            //    await next();
+            //});
+
+            //app.Run(async context => {
+            //    await context.Response.WriteAsync("asdsad");
+            //});
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseRouting();
+            
+            app.UseEndpoints(endpoints =>
+            {
+                //for gernel routes
+                //endpoints.MapGet("/", async context => {
+                //   await context.Response.WriteAsync("Hello World!");
+                //});
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
