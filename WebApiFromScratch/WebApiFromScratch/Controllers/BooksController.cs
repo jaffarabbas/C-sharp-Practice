8a1eef31d75ec8d4ca0d6b3e2cf45feac20792b3
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-31 23:42:54 +0500

    Commited File : BooksController.cs at 7/31/2022 11:42:53 PM

---
+++ WebApiFromScratch/WebApiFromScratch/Controllers/BooksController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiFromScratch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [Route("{id:int:min(10)}")]
+        public string GetById(int id)
+        {
+            return "Id : " + id;
+        }
+        
+
+        [Route("{id}")]
+        public string GetByString(string id)
+        {
+            return "Id : " + id;
+        }
+    }
+}
