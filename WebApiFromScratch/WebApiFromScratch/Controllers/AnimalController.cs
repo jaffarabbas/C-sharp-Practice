5364be05616564bbf85ad510c155f97b24960767
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-02 17:52:42 +0500

    Commited File : AnimalController.cs at 8/2/2022 5:52:41 PM

---
+++ WebApiFromScratch/WebApiFromScratch/Controllers/AnimalController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApiFromScratch.Models;
+
+namespace WebApiFromScratch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalController : ControllerBase
+    {
+        public IActionResult GetAnimal()
+        {
+            var animal = new List<Animal>()
+            {
+                new Animal(){id = 1, name = "cat"},
+            };
+            return Ok (animal);
+        }
+    }
+}
