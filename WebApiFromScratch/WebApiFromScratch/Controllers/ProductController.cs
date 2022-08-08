b6a1be01b992d1b4fc3658138f7155451958aae6
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-08 16:38:43 +0500

    Commited File : ProductController.cs at 8/8/2022 4:38:42 PM

---
+++ WebApiFromScratch/WebApiFromScratch/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiFromScratch.Models;
+using WebApiFromScratch.Repository;
+
+namespace WebApiFromScratch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductRepository _productRepository;
+        
+        public ProductController()
+        {
+            _productRepository = new ProductRepository();
+        }
+        
+        [HttpPost("")]
+        public IActionResult AddProducts(ProductModel product)
+        {
+            _productRepository.AddProduct(product);
+            var products = _productRepository.GetProducts();
+            return Ok(products);
+        }
+    }
+}
