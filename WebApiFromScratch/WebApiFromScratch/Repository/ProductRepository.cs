d682c97cfca10d587e436153290ab35fc7b230af
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-08 16:38:45 +0500

    Commited File : ProductRepository.cs at 8/8/2022 4:38:44 PM

---
+++ WebApiFromScratch/WebApiFromScratch/Repository/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Generic;
+using WebApiFromScratch.Models;
+
+namespace WebApiFromScratch.Repository
+{
+    public class ProductRepository
+    {
+        private List<ProductModel> list = new List<ProductModel>();
+        public int AddProduct(ProductModel product)
+        {
+            product.Id = list.Count + 1;
+            list.Add(product);
+            return product.Id;
+        }
+
+        public List<ProductModel> GetProducts()
+        {
+            return list;
+        }
+    }
+}
