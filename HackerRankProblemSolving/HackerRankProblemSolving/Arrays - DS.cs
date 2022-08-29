8a29ea7c756312452e735d06d3eec9854ae3e1c1
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-29 17:26:54 +0500

    Inserted HackerRankProblemSolving/Arrays - DS.cs at 29/08/2022 17:26:54

---
+++ HackerRankProblemSolving/HackerRankProblemSolving/Arrays - DS.cs
@@ -0,0 +1,50 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'reverseArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static List<int> reverseArray(List<int> a)
+    {
+        a.Reverse();
+        return a;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> res = Result.reverseArray(arr);
+
+        textWriter.WriteLine(String.Join(" ", res));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
