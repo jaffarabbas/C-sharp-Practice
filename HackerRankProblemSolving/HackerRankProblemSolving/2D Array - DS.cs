e8afb82120cc7a7abbfb88ccf395a79fa207452c
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-29 17:26:54 +0500

    Inserted HackerRankProblemSolving/2D Array - DS.cs at 29/08/2022 17:26:50

---
+++ HackerRankProblemSolving/HackerRankProblemSolving/2D Array - DS.cs
@@ -0,0 +1,66 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+class Result
+{
+
+    /*
+     * Complete the 'hourglassSum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int hourglassSum(List<List<int>> arr)
+    {
+        int max = int.MinValue;
+        for (int i = 0; i < arr.Count - 2; i++)
+        {
+            for (int j = 0; j < arr[i].Count - 2; j++)
+            {
+                int sum = arr[i][j] + arr[i][j + 1] +
+                    arr[i][j + 2] + arr[i + 1][j + 1] +
+                    arr[i + 2][j] + arr[i + 2][j + 1] +
+                    arr[i + 2][j + 2];
+                if (sum > max)
+                {
+                    max = sum;
+                }
+            }
+        }
+        return max;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.hourglassSum(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
