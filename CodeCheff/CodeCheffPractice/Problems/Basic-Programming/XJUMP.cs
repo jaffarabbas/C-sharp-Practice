8bcaa96a6cb4b47bc4b3bdcc9c6f7e5648ef69ae
Jaffar Abbas <gamaportal8@gmail.com>
2023-01-13 10:34:06 +0500

    1-13-2023

---
+++ CodeCheff/CodeCheffPractice/Problems/Basic-Programming/XJUMP.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCheffPractice.Problems.Basic_Programming
+{
+    public class XJUMP
+    {
+        public static int code()
+        {
+            int i, j;
+            i = Convert.ToInt32(Console.ReadLine());
+            j = Convert.ToInt32(Console.ReadLine());
+            int steps = i / j;
+            int stairs = i % j;
+            return stairs + steps;
+        }
+
+        public void submission()
+        {
+            int n = Console.Read();
+            int i = 1;
+            while (i <= n)
+            {
+                code();
+                i++;
+            }
+        }
+    }
+}
--- CodeCheff/CodeCheffPractice/Program.cs
+++ CodeCheff/CodeCheffPractice/Program.cs
@@ -1,9 +1,21 @@
-﻿using System;
+﻿using CodeCheffPractice.Problems.Basic_Programming;
+using System;
 
 public class Test
 {
+	//test case problem function
+	public static void TestCode()
+	{
+		int n = Console.Read();
+		int i = 1;
+		while (i<=n)
+		{
+            XJUMP.code();
+            i++;
+		}
+	}
 	public static void Main()
 	{
-		// your code goes here
+		TestCode();
 	}
 }
