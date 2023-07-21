2aab241c68affdec48ac9e011cfc0015508e8fb2
jaffarabbas <gamaportal8@gmail.com>
2023-07-21 23:47:02 +0500

    Commited File : Program.cs at 21/07/2023 11:47:01 pm

--- ImterviewPractice/ImterviewPractice/Program.cs
+++ ImterviewPractice/ImterviewPractice/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ImterviewPractice;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Test ob = new Test(10);
+            Console.WriteLine(ob.get());
+        }
+    }
+}
