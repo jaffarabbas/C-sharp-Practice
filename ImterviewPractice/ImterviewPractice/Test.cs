77ba731004969a9c22fd3eb00ec77018d6f3e60b
jaffarabbas <gamaportal8@gmail.com>
2023-07-21 23:47:01 +0500

    Commited File : Test.cs at 21/07/2023 11:47:01 pm

---
+++ ImterviewPractice/ImterviewPractice/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImterviewPractice
+{
+    public class Test : Abstract
+    {
+        public int a = 3;
+        public Test(int n) : base(n)
+        {
+            this.n = n;
+        }
+
+        public override int get()
+        {
+            return n;
+        }
+    }
+}
