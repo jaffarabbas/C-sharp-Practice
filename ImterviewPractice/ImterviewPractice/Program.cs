ab2715e6a80bbd10d43696b1a151d3879f6d8959
jaffarabbas <gamaportal8@gmail.com>
2023-07-24 18:37:45 +0500

    Commited File : Program.cs at 24/07/2023 6:37:45 pm

--- ImterviewPractice/ImterviewPractice/Program.cs
+++ ImterviewPractice/ImterviewPractice/Program.cs
@@ -10,14 +10,16 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             Abstract obj = new Test(10);
             Test obj2 = new Test(10);
-            //Console.WriteLine(obj.get());
+            Console.WriteLine(obj.get());
             //Console.WriteLine(obj.s);
             //Console.WriteLine(obj.calculate(4));
             //Console.WriteLine(obj2.calculate(3));
             //Console.WriteLine(); ;
             //obj2.rec(10);
-            int[] arr = {2,3,5};
-            Console.WriteLine(obj2.feb(arr, 3));
+            //int[] arr = {2,3,5};
+            //Console.WriteLine(obj2.feb(arr, 3));
+            Check O = new Check();
+            Console.WriteLine(O.isPrime(2));
         }
     }
 }
