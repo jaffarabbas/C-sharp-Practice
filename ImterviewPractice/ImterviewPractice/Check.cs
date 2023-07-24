dcbf020f33ba1ddc31cf37ec8dbb014ac4881c0d
jaffarabbas <gamaportal8@gmail.com>
2023-07-24 18:37:44 +0500

    Commited File : Check.cs at 24/07/2023 6:37:43 pm

---
+++ ImterviewPractice/ImterviewPractice/Check.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImterviewPractice
+{
+    public class Check : Check2
+    {
+        public override void get()
+        {
+            Console.WriteLine("sadasd");
+        }
+
+        public bool isPrime(int n)
+        {
+            if(n <= 1) return false;
+            if(n <= 3) return true;
+
+            for(int i = 2; i < Math.Sqrt(n); i++) {
+                if (n % i == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
