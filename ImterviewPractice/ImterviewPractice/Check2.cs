ab983f4b915d7044310456998dffa4695f3e84a3
jaffarabbas <gamaportal8@gmail.com>
2023-07-24 18:37:44 +0500

    Commited File : Check2.cs at 24/07/2023 6:37:44 pm

---
+++ ImterviewPractice/ImterviewPractice/Check2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImterviewPractice
+{
+    public class Check2
+    {
+        public virtual void get()
+        {
+            Console.WriteLine("asdasd");
+        }
+    }
+}
