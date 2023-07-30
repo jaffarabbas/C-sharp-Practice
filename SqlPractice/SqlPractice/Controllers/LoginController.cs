d701faaf413546fd6f0ae5386ff68d82aa8b9d89
jaffarabbas <gamaportal8@gmail.com>
2023-07-30 19:00:31 +0500

    Commited File : LoginController.cs at 30/07/2023 7:00:31 pm

--- SqlPractice/SqlPractice/Controllers/LoginController.cs
+++ SqlPractice/SqlPractice/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SqlPractice.Models;
 
 namespace SqlPractice.Controllers
 {
@@ -12,6 +13,37 @@ namespace SqlPractice.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(customer c)
+        {
+            using(dbMvcEntities db = new dbMvcEntities())
+            {
+                if (ModelState.IsValid == true)
+                {
+                    var data = db.customers.FirstOrDefault(m => m.email == c.email && m.password == c.password);
+                    if (data == null)
+                    {
+                        ViewBag.ErrorMessage = "Login Failed";
+                        return View();
+                    }
+                    else
+                    {
+                        Session["id"] = data.id;
+                        Session["name"] = data.name;
+                        Session["email"] = data.email;
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index","Home");
         }
     }
 }
