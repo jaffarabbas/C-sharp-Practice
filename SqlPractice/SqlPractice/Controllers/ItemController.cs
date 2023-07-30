e9f2574fe3b3775d997599ba452af962a0acd9a4
jaffarabbas <gamaportal8@gmail.com>
2023-07-30 20:22:35 +0500

    Commited File : ItemController.cs at 30/07/2023 8:22:35 pm

---
+++ SqlPractice/SqlPractice/Controllers/ItemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SqlPractice.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
