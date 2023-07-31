ffaa5a655d6c67820dd1c08e6849a422c4e79831
jaffarabbas <gamaportal8@gmail.com>
2023-07-31 18:15:18 +0500

    Commited File : SalesController.cs at 31/07/2023 6:15:17 pm

--- SqlPractice/SqlPractice/Controllers/SalesController.cs
+++ SqlPractice/SqlPractice/Controllers/SalesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SqlPractice.Models;
 
 namespace SqlPractice.Controllers
 {
@@ -10,6 +11,17 @@ namespace SqlPractice.Controllers
     {
         // GET: Sales
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(sale s)
+        {
+            return View();
+        }
+
+        public ActionResult List()
         {
             return View();
         }
