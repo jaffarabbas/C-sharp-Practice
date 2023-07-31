d12d89dd966be264ecd383f8ca064bd48d24d21c
jaffarabbas <gamaportal8@gmail.com>
2023-07-31 22:58:29 +0500

    Commited File : HomeController.cs at 31/07/2023 10:58:29 pm

--- SqlPractice/SqlPractice/Controllers/HomeController.cs
+++ SqlPractice/SqlPractice/Controllers/HomeController.cs
@@ -19,19 +19,5 @@ namespace SqlPractice.Controllers
                 return RedirectToAction("Index","Login");
             }
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
