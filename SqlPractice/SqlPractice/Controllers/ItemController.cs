f8ad9c75afb52181b095cb57bdd4ec8b21c05c3b
jaffarabbas <gamaportal8@gmail.com>
2023-07-31 22:50:56 +0500

    Commited File : ItemController.cs at 31/07/2023 10:50:56 pm

--- SqlPractice/SqlPractice/Controllers/ItemController.cs
+++ SqlPractice/SqlPractice/Controllers/ItemController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SqlPractice.Models;
 
 namespace SqlPractice.Controllers
 {
@@ -11,7 +12,49 @@ namespace SqlPractice.Controllers
         // GET: Item
         public ActionResult Index()
         {
-            return View();
+            if (Session["email"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Index(item e)
+        {
+            using (dbMvcEntities db = new dbMvcEntities())
+            {
+                db.items.Add(e);
+                var data = db.SaveChanges();
+                if (data > 0)
+                {
+                    TempData["InsertMessage"] = "<script>alert('Sales Data Inserted')</script>";
+                    return RedirectToAction("Index", "Item");
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "<script>alert('Sales Data Is Not Inserted')</script>";
+                    return View();
+                }
+            }
+        }
+
+        public IEnumerable<item> GetItems()
+        {
+            using (dbMvcEntities db = new dbMvcEntities())
+            {
+                var result = db.items.ToList();
+                return result;
+            }
+        }
+
+        public PartialViewResult List()
+        {
+            var data = GetItems();
+            return PartialView(data);
         }
     }
 }
