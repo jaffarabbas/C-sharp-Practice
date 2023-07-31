03920b1f84555a61a41b1f94403cf3226baf78bd
jaffarabbas <gamaportal8@gmail.com>
2023-07-31 18:15:17 +0500

    Commited File : RegisterController.cs at 31/07/2023 6:15:17 pm

--- SqlPractice/SqlPractice/Controllers/RegisterController.cs
+++ SqlPractice/SqlPractice/Controllers/RegisterController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SqlPractice.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,30 @@ namespace SqlPractice.Controllers
         // GET: Register
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(customer c)
+        {
+            using (dbMvcEntities db = new dbMvcEntities())
+            {
+                if(ModelState.IsValid == true)
+                {
+                    db.customers.Add(c);
+                    var isDone = db.SaveChanges();
+                    if (isDone > 0)
+                    {
+                        TempData["AccountCreated"] = "<script>alert('Account Created')</script>";
+                        return RedirectToAction("Index", "Login");
+                    }
+                    else
+                    {
+                        ViewBag.ErrorMessage = "Register Failed!!";
+                        return View();
+                    }
+                }
+            }
             return View();
         }
     }
