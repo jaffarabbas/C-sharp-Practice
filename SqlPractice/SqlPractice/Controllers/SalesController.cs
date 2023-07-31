eb334ed2263c9c573a4137c4ab252bc3512730da
jaffarabbas <gamaportal8@gmail.com>
2023-07-31 22:29:58 +0500

    Commited File : SalesController.cs at 31/07/2023 10:29:57 pm

--- SqlPractice/SqlPractice/Controllers/SalesController.cs
+++ SqlPractice/SqlPractice/Controllers/SalesController.cs
@@ -12,18 +12,52 @@ namespace SqlPractice.Controllers
         // GET: Sales
         public ActionResult Index()
         {
+            using (dbMvcEntities db = new dbMvcEntities())
+            {
+                var selectCustomer = db.customers.ToList();
+                var selectItems = db.items.ToList();
+                ViewBag.CustomerList = new SelectList(selectCustomer, "id", "name");
+                ViewBag.ItemList = new SelectList(selectItems, "itemId", "iname");
+            }
             return View();
         }
 
         [HttpPost]
         public ActionResult Index(sale s)
         {
-            return View();
+            using (dbMvcEntities db = new dbMvcEntities())
+            {
+                db.sales.Add(s);
+                var data = db.SaveChanges();
+                if(data > 0)
+                {
+                    TempData["InsertMessage"] = "<script>alert('Sales Data Inserted')</script>";
+                    return RedirectToAction("Index","Sales");
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "<script>alert('Sales Data Is Not Inserted')</script>";
+                    return View();
+                }
+            }
         }
 
-        public ActionResult List()
+        public IEnumerable<sale> GetSales()
         {
-            return View();
+            using (dbMvcEntities db = new dbMvcEntities())
+            {
+                var result =  db.sales
+                        .Include("customer") // Load the customer entity
+                        .Include("item") // Load the item entity
+                        .ToList();
+                return result;
+            }
+        }
+
+        public PartialViewResult List()
+        {
+            var data = GetSales();
+            return PartialView(data);
         }
     }
 }
